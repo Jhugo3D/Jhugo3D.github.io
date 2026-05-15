63bda3ec4c6ca71ef778471eb6841577ece4c6d3
Jhugo3D <soportejhugo3d@gmail.com>
2026-05-15 23:56:12 +0200

    Astro

--- jhugo3d/astro.config.mjs
+++ jhugo3d/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jhugo3d.es',
+  site: 'https://jhugo3d.github.io',
+  base: '/',
   image: {
     remotePatterns: [{ protocol: 'https' }],
   },
